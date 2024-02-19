8709a247e66bf003da9af958960b5a9039d0ad32
Silfr-Coder <157309052+Silfr-Coder@users.noreply.github.com>
2024-02-19 11:22:20 +0000

    create initial component structure

--- interactiveglossary/src/App.js
+++ interactiveglossary/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Buttons from "./Components/Buttons";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="button-links">
+          Links will appear here
+          <Buttons />
+        </div>
+        <div className="concept-text">Concept text will appear here</div>
       </header>
     </div>
   );
---
+++ interactiveglossary/src/Components/Buttons.jsx
@@ -0,0 +1,3 @@
+//this will be the component for the buttons and links down the left-hand side
+
+export default function Buttons() {}
